4f3a22a09f69188f38578614b84ceb7506b25bac
07kagan07 <kagancoskun32@gmail.com>
2022-12-28 21:49:02 +0300

    db connection

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,13 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import barcodeData from "../barkod.json";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import axios from "axios";
 
 export default function CameraExam({ scannedData, setScannedData }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  // const [barcodeData, setBarcodeData] = useState();
+
   const Stack = createNativeStackNavigator();
+
+  const getData = async (data) => {
+    const req = await axios
+      .post(
+        "https://data.mongodb-api.com/app/data-iiffl/endpoint/data/v1/action/findOne",
+        {
+          collection: "Barcode",
+          database: "DontWaste",
+          dataSource: "DontWaste",
+          filter: { BarcodeNo: data },
+        },
+        {
+          headers: {
+            Accept: "*/*",
+            "Content-Type": "application/json",
+            "Access-Control-Request-Headers": "*",
+            "Accept-Encoding": "application/ejson",
+            "api-key":
+              "S3CE4uKgwIhv23UJrlBcxe7sLVBA2jVE8kZdH41MNkgQiwr6jBFu4T9reXPlT8mr",
+          },
+        }
+      )
+      .then((res) => {
+        return res.data.document;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    return req;
+  };
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -18,16 +51,12 @@ export default function CameraExam({ scannedData, setScannedData }) {
     getBarCodeScannerPermissions();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
-
-    const barcode = barcodeData?.find((barcode) => barcode.BarcodeNo === data);
-
-    // console.log("Product Detail=>", barcode);
-
-    barcode
-      ? setScannedData([...scannedData, barcode])
-      : alert(`Ürün Bulunamadı!`);
+    const barcodeData = await getData(data);
+    barcodeData
+      ? setScannedData([...scannedData, barcodeData])
+      : alert("Ürün Bulunamadı");
   };
 
   if (hasPermission === null) {
